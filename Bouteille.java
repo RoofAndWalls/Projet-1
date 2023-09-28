8fb078ecd33921767f11bbadf0c2d0f23466d813
RoofAndWalls-g08 <115184251+RoofAndWalls@users.noreply.github.com>
2023-09-28 14:03:01 -0400

    done

--- Bouteille.java
+++ Bouteille.java
@@ -1,3 +1,19 @@
 public class Bouteille {
+
+    String type = "Empty bottle";
+    String shape = "Cercle";
+    String size = "1cm";
     
+    
+    public void specifyType(String type) {
+        this.type = type;
+    }
+
+    public void specifyShape(String shape){
+        this.shape = shape;
+    }
+
+    public void specifySize(String size) {
+        this.size = size;
+    }
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,25 @@
 public class Main {
     public static void main(String[] args) {
         Bouteille Bt = new Bouteille();
-    }
+        Bouteille Bt2 =  new Bouteille();
+
+ Bt.specifyShape("Cylindre"); 
+ Bt.specifyType("Bouteille d'eau");
+ Bt.specifySize("10cm");
+
+
+Bt2.specifyShape("Hexagon"); 
+Bt2.specifyType("Bouteille jus");
+Bt2.specifySize("12cm");
+
+System.out.println("Bt");
+System.out.println("shape:" + Bt.shape);
+System.out.println("type:" + Bt.type);
+System.out.println("size:" + Bt.size);
+
+System.out.println("Bt2");
+System.out.println("shape:" + Bt2.shape);
+System.out.println("type:" + Bt2.type);
+System.out.println("size:" + Bt2.size);
+}
 }
