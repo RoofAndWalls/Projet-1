56139b138bf451ba98b683f551ee3b41867a3302
RoofAndWalls-g08 <115184251+RoofAndWalls@users.noreply.github.com>
2023-10-03 10:27:45 -0400

    b

--- Bouteille.java
+++ Bouteille.java
@@ -3,8 +3,22 @@ public class Bouteille {
     String type = "Empty bottle";
     String shape = "Cercle";
     String size = "1cm";
-    
-    
+    double side = 1.0;
+    double area;
+    double perimeter; 
+
+Bouteille(){
+    this(1.0);
+}
+
+Bouteille (double side) {
+    this.side = side;
+
+    calculateArea();
+    calculatePerimeter();
+
+}
+
     public void specifyType(String type) {
         this.type = type;
     }
@@ -16,4 +30,23 @@ public class Bouteille {
     public void specifySize(String size) {
         this.size = size;
     }
+
+public void calculateArea() {
+    area = side/2*side/2 * 3.14;
+}
+
+public void calculatePerimeter() {
+    perimeter = side*3.14;
+}
+
+@Override
+public String toString() {
+    String output = "";
+    output += "type?" + this.type + "; ";
+    output += "shape" + this.shape + "; ";
+    output += "size" + this.size + "\n";
+}
+
+
+
 }
